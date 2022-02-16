dceaab0b9961b42aa8f2c48bfe49e99934e1d819
jay ber <jayber1@gmail.com>
2022-02-16 11:37:52 +0200

    complete project

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+from ntpath import join
+import winsound
+from numpy import append
+import time
+
+
 morse_data = {
     'A': '.-' ,
     'B': '-..',
@@ -27,4 +33,29 @@ morse_data = {
     'Z': '--..',
     }
 
-input('enter text')
+
+
+user_input = input('enter text \n').upper()
+
+# print(user_input)
+morse_string = ""
+for i in user_input:
+    if i in morse_data:
+        to_add = morse_data[i]
+        morse_string = morse_string + " " + morse_data[i]
+
+print(morse_string)
+
+for i in morse_string:
+    print(i)
+    if i == ".":
+        winsound.Beep(1500,200)     
+        time.sleep(0.1)
+    elif i == "-":
+        winsound.Beep(1500,500)
+        time.sleep(0.1)
+    else:
+        time.sleep(0.4)
+
+
+print(morse_string)
